78b0e28c3f98ad2c101d4351d225000e51d4d412
hwinkr <dnddl8280@naver.com>
2023-09-13 19:21:46 +0900

    Chore: Global Style & 라우팅 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyleProvider from './styles/GlobalStyleProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyleProvider>
+      <App />
+    </GlobalStyleProvider>
   </React.StrictMode>,
 );
